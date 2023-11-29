0301883e43c9d36fac993e6fffb065254eab1fa9
Sofia Katzman <katzmansof@gmail.com>
2023-11-29 18:17:18 -0500

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  SafeAreaView, 
+  Image, 
+  TouchableHighlight,
+  Button,
+  Alert
+} from 'react-native';
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text> I love harri.</Text>
+
+      <Button  color="orange" title="Click Me" onPress={()=>Alert.alert("My title", "My message", [
+        {text: "yes", onPress: () => console.log("yes clicked")},
+        {text: "No", onPress: () => console.log("no clicked")}
+        ])}/>
+            
+      <Button  color="blue" title="Click Me2" onPress={()=>Alert.prompt("My title", "My message", text =>  console.log(text))}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ README.md
@@ -0,0 +1,11 @@
+to start application on device emulator
+download Expo Go
+on root directory run
+
+> npm install
+> expo-cli start --tunnel
+
+scan qr code with ios or android device
+click on "Open with Expo Go"
+
+application should
