12a9c6e1b6334c12c0c31ff6d8c05168737fb158
Sofia Katzman <katzmansof@gmail.com>
2023-11-29 20:10:30 -0500

    add basic text and styling

--- App.js
+++ App.js
@@ -1,38 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { 
-  StyleSheet, 
+import React from "react";
+import {
+  SafeAreaView,
   Text, 
-  View, 
-  SafeAreaView, 
-  Image, 
-  TouchableHighlight,
-  Button,
-  Alert
+  StyleSheet,
+  View
 } from 'react-native';
 
 
-export default function App() {
+const App = () => {
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text> I love harri.</Text>
-
-      <Button  color="orange" title="Click Me" onPress={()=>Alert.alert("My title", "My message", [
-        {text: "yes", onPress: () => console.log("yes clicked")},
-        {text: "No", onPress: () => console.log("no clicked")}
-        ])}/>
-            
-      <Button  color="blue" title="Click Me2" onPress={()=>Alert.prompt("My title", "My message", text =>  console.log(text))}/>
-      <StatusBar style="auto" />
+  return(
+    <SafeAreaView style={styles.wrapper} >
+      <View style={styles.container}>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like: 5</Text>
+          <View style={styles.highLowWrapper}> 
+            <Text style={styles.highLow}>High: 8 </Text>
+            <Text style={styles.highLow}>Low: 6</Text>
+          </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+            <Text style={styles.description}>It's sunny</Text>
+            <Text style={styles.message}>It's perfect t-Shirt weather</Text>
+          </View>
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'pink',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  temp: {
+    color: "black",
+    fontSize: 48,
+  },
+  feels: {
+    color: "black",
+    fontSize: 30,
+  },
+  highLow: {
+    color: "black",
+    fontSize: 20,
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+  }
+  ,
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignContent: "flex-start",
+  },
+  description: {
+    fontSize: 38,
+  },
+  message: {
+    fontSize: 30,
+  },
+
+})
+
+export default App;
